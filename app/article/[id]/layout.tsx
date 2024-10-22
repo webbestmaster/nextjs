5f3362c8fafe3cbd6bcce3a3f5c9388d173676f2
Dzmitry Turautsou <web.best.master@gmail.com>
2024-10-22 12:25:49 +0200

    update deps

--- app/article/[id]/layout.tsx
+++ app/article/[id]/layout.tsx
@@ -2,17 +2,19 @@ import type {JSX, ReactNode} from "react";
 
 type PropsType = Readonly<{
     children: ReactNode;
-    params: {id: string};
+    params: Promise<{id: string}>;
 }>;
 
 export function generateStaticParams(): Array<{id: string}> {
     return [{id: "1"}, {id: "2"}, {id: "3"}];
 }
 
-export default function Article(props: PropsType): JSX.Element {
+export default async function Article(props: PropsType): Promise<JSX.Element> {
+    const {id} = await props.params;
+
     return (
         <>
-            <h1>article: {props.params.id}</h1>
+            <h1>article: {id}</h1>
             <div>{props.children}</div>
         </>
     );
