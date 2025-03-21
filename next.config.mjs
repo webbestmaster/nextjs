011661140f89c9a64606342809e4d0ab3ae4af3c
dim <web.best.master@gmail.com>
2025-03-21 19:29:35 +0100

    WIP

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", // Check "out" folder
+    // output: "export", // Check "out" folder
 };
 
 export default nextConfig;
